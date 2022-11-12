7ac9ca085ff65976aed506dbfedf8b15b649128a
Artur <Pletnyk95@mail.ru>
2022-11-12 20:42:40 +0300

    Задача8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 0;
+while(num1 != num)    
+{
+    num1 += 1;
+    int num2 = num1 % 2;
+    if (num2 == 0)
+        Console.WriteLine(num1);
+}
+Console.Write("Exit.");
+Console.ReadLine();
